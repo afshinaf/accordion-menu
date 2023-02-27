566b52a7323686dc6c40d38ee239da7100ec650b
Afshin <af.afshin92@gmail.com>
2023-02-27 19:16:49 +0330

    Create header of menu

---
+++ src/components/menu.js
@@ -0,0 +1,13 @@
+import Header from './header';
+
+const Menu = () => {
+    return (
+        <>
+            <div className="box d-flex flex-column">
+                <Header />
+            </div>
+        </>
+    )
+}
+
+export default Menu
