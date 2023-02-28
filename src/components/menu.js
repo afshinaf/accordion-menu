898c5e9b472b1e5d10cc9119844e817b30319afa
Afshin <af.afshin92@gmail.com>
2023-02-28 16:37:50 +0330

    Create accordion menu component

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,10 +1,12 @@
 import Header from './header';
+import AccordionMenu from "./accordion-menu";
 
 const Menu = () => {
     return (
         <>
             <div className="box d-flex flex-column">
                 <Header />
+                <AccordionMenu />
             </div>
         </>
     )
